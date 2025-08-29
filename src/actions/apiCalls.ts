6bc420c507dc78276c7cd9b34874a11fcc67bcaf
Rob <86649932+GuidDotEmpty@users.noreply.github.com>
2025-08-29 15:58:59 +0200

    fix: update API URL to reflect unavailable endpoint in getPosts function

--- src/actions/apiCalls.ts
+++ src/actions/apiCalls.ts
@@ -1,7 +1,7 @@
 import { BlogPost } from '@/types/BlogPost';
 
 export async function getPosts(): Promise<BlogPost[]> {
-  const url = 'localhost:1/api/posts'; //
+  const url = 'localhost:1/api/notworkingurl'; //Example of an unavailable url in build
   const data = await fetch(url);
   return await data.json();
 }
